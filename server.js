f79d7a18cabae1098d6fd7a2e8e4811bbe3cbd31
Steve Braaksma <stevebraaksma@hotmail.com>
2021-10-07 21:14:03 -0500

    begin express setup

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+//
+//
+
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+
+require('dotenv').config();
+

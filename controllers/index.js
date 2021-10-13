cbc1f7a61fa3bb274bfcd3659aeb17bab0dcde0d
Steve Braaksma <stevebraaksma@hotmail.com>
2021-10-13 13:56:16 -0500

    auth test route

--- controllers/index.js
+++ controllers/index.js
@@ -6,8 +6,8 @@ indexRouter.get('/index', (req, res) => {
     res.render('home.ejs');
 });
 
-indexRouter.get('/callback', (req, res) => {
-    res.redirect('/index');
-})
+// indexRouter.get('/callback', (req, res) => {
+//     res.redirect('/index');
+// })
 
 module.exports = indexRouter;

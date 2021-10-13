d2826030e4203a54ff8bec598716a9ebb8557031
Steve Braaksma <stevebraaksma@hotmail.com>
2021-10-13 12:41:37 -0500

    update env vars for heroku

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ const { auth, requiresAuth } = require('express-openid-connect');
 const config = {
   authRequired: false,
   auth0Logout: true,
-  secret: process.env.SECRET,
-  baseURL: process.env.BASE_URL,
-  clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUER_BASE_URL,
+  secret: process.env.AUTH0_CLIENT_SECRET,
+  baseURL: process.env.AUTH0_CALLBACK_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_DOMAIN,
 };
 
 const DATABASE_URL = process.env.DATABASE_URL;

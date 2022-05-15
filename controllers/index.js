5065f7cf5fc182030764d8b33c51d734b7ebdf4b
Steve Braaksma <stevebraaksma@hotmail.com>
2022-05-14 20:47:45 -0500

    change 'index' to 'home'

--- controllers/index.js
+++ controllers/index.js
@@ -2,19 +2,19 @@ const express = require('express');
 const indexRouter = express.Router();
 const { auth, requiresAuth } = require('express-openid-connect');
 
-indexRouter.get('/', (req, res) => {
-    res.redirect('/landing');
-});
+// indexRouter.get('/', (req, res) => {
+//     res.redirect('/home');
+// });
 
 
 // original home page
-indexRouter.get('/index', (req, res) => {
+indexRouter.get('/home', (req, res) => {
     res.render('home.ejs');
 });
 
 
 // future new home
-indexRouter.get('/newlanding', (req, res) => {
+indexRouter.get('/', (req, res) => {
     res.render('landing.ejs');
 });
 

5d9566b090a5242a80868f0604e2da985583b08b
Steve Braaksma <stevebraaksma@hotmail.com>
2022-09-03 17:03:40 -0500

    function to add price to bomdata

--- controllers/excelprojects.js
+++ controllers/excelprojects.js
@@ -6,6 +6,8 @@ const excelprojectsRouter = express.Router();
 const { auth, requiresAuth } = require('express-openid-connect');
 const Project = require('../models/project');
 
+let bomData = [];
+
 // excel new route
 excelprojectsRouter.get('/excelprojects/excelnew', requiresAuth(), (req, res) => {
     res.render('excelnew-new.ejs'), {
@@ -17,7 +19,22 @@ excelprojectsRouter.get('/excelprojects/excelnew', requiresAuth(), (req, res) =>
 excelprojectsRouter.post('/excelprojects/', upload.single('uploaded_file'), function (req, res) { 
     const myFile = req.file.filename;
     const workSheetsFromFile = xlsx.parse(`public/${myFile}`);
-    const bomData = workSheetsFromFile[0].data;
+    // const 
+    bomData = workSheetsFromFile[0].data;
+
+
+    // within this part is where 
+
+
+
+
+
+    // console.log(bomData);
+    testerHello();
+    addPriceStockToBomData ()
+
+
+
     res.render('excelformsubmit.ejs', {
         project: bomData,
         currentLoggedInUser: req.oidc.user.name
@@ -29,5 +46,45 @@ excelprojectsRouter.post('/excelprojects/', upload.single('uploaded_file'), func
         res.redirect('/projects');
     });
 });
+
+function testerHello () {
+    console.log(bomData)
+    bomData[0][0] = 'surpraise';
+    console.log(bomData)
+
+}
+
+function getPriceStock () {
+
+    
+
+
+};
+
+function addPriceStockToBomData () {
+
+    // Change this so that it adds to the array instead of changing an item
+    
+    // add title fields
+    bomData[0][3] = 'Price';
+    bomData[0][4] = 'Stock';
+
+
+
+    // (before this appears on page, would have to update the data model)
+
+
+    for (let i = 1; i < bomData.length; i++) {
+        // add in the price in the 3rd position of each item
+        bomData[i][3] = 'price';
+        // and in the stock in the 4th position of each item
+        bomData[i][4] = 'stock';
+        console.log('woof')
+    };
+    console.log(bomData);
+
+
+};
+
 
 module.exports = excelprojectsRouter;

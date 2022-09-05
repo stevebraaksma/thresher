6a07a31667b1997dae8257108358b9353328f01c
Steve Braaksma <stevebraaksma@hotmail.com>
2022-09-05 02:15:04 -0500

    select proper price break

--- controllers/excelprojects.js
+++ controllers/excelprojects.js
@@ -10,6 +10,47 @@ const axios = require('axios');
 
 let bomData = [];
 
+
+function getPriceStock () {
+    for (let i = 1; i < bomData.length-1; i++) {
+        // bomData[i][3] = 'default';
+        // let holdingVariable = [];
+        // let bomData2 = bomData;
+        let currentPartNum = bomData[i][0];
+        axios({
+            method: 'post',
+            url: "https://api.mouser.com/api/v1/search/keyword?apiKey=53cd927d-3725-4cce-aaa8-50851d7c13f6",
+            data: {
+                SearchByKeywordRequest: {
+                            keyword: currentPartNum
+                    }
+            }
+        })
+        .then( (response)=>{
+            let currentBomQty = bomData[i][1];
+            let actualPrice = '';
+            for (let iterator = 0; currentBomQty >= response.data.SearchResults.Parts[0].PriceBreaks[iterator].Quantity; iterator++) {
+                actualPrice = response.data.SearchResults.Parts[0].PriceBreaks[iterator].Price;
+            };
+            bomData[i][3] = actualPrice;
+            bomData[i][4] = i;
+            console.log(bomData);
+            })
+        .catch((err) => {
+            console.log(err);
+        })
+    };
+    bomData.pop();  // remove the extra empty item added
+};
+function addPriceStockLabelsToBomData () {
+    bomData[0][3] = 'Price';
+    bomData[0][4] = 'Stock';
+
+    // (before this appears on page, would have to update the data model)
+
+};
+
+
 // excel new route
 excelprojectsRouter.get('/excelprojects/excelnew', requiresAuth(), (req, res) => {
     res.render('excelnew-new.ejs'), {
@@ -24,17 +65,9 @@ excelprojectsRouter.post('/excelprojects/', upload.single('uploaded_file'), func
     // const 
     bomData = workSheetsFromFile[0].data;
 
-
-    // within this part is where 
-
-
-
-
-
-    // console.log(bomData);
-    testerHello();
-    addPriceStockToBomData();
+    
     getPriceStock();
+    addPriceStockLabelsToBomData();
 
 
     res.render('excelformsubmit.ejs', {
@@ -49,63 +82,6 @@ excelprojectsRouter.post('/excelprojects/', upload.single('uploaded_file'), func
     });
 });
 
-function testerHello () {
-    console.log(bomData)
-    bomData[0][0] = 'surpraise';
-    console.log(bomData)
-
-}
-
-function getPriceStock () {
-    for (let i = 1; i < bomData.length; i++) {
-        let currentPartNum = bomData[i][0];
-
-        axios({
-            method: 'post',
-            url: "https://api.mouser.com/api/v1/search/keyword?apiKey=53cd927d-3725-4cce-aaa8-50851d7c13f6",
-            data: {
-                SearchByKeywordRequest: {
-                            keyword: currentPartNum
-                    }
-            }
-        })
-        .then( (response)=>{
-            // addPriceStockToBomData ();
-            console.log(response);
-
-
-            })
-        .catch((err) => {
-            console.log(err);
-        })
-
-
-    };
-};
-
-function addPriceStockToBomData () {
-
-    // Change this so that it adds to the array instead of changing an item
-    
-    // add title fields
-    bomData[0][3] = 'Price';
-    bomData[0][4] = 'Stock';
-
-
-
-    // (before this appears on page, would have to update the data model)
-
-
-    for (let i = 1; i < bomData.length; i++) {
-        // add in the price in the 3rd position of each item
-        bomData[i][3] = 'price';
-        // add in the stock in the 4th position of each item
-        bomData[i][4] = 'stock';
-    };
-    console.log(bomData);
-
-
-};
 
 
 module.exports = excelprojectsRouter;
